1478a902669ceea5b9c707adef83685901021696
Yannick Menager <4215831-ymenager@users.noreply.gitlab.com>
2021-01-02 18:45:33 -0800

    In progress

--- formula/emt.rb
+++ formula/emt.rb
@@ -1,6 +1,6 @@
 require "formula"
 
-class Tools < Formula
+class Emt < Formula
   desc "Enhanced Mule Tools CLI"
   homepage "https://www.enhanced-mule.com"
   url "https://repo1.maven.org/maven2/com/aeontronix/enhanced-mule/enhanced-mule-tools-cli/1.2.7-beta3/enhanced-mule-tools-cli-1.2.7-beta3-dist.tbz2"
