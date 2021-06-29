cf2af361f1ee1d7eb2f39c59909210bae60cd8e7
ymenager <ymenager@users.noreply.github.com>
2021-06-29 08:47:14 -0700

    update

--- formula/emt.rb
+++ formula/emt.rb
@@ -4,7 +4,7 @@ class Emt < Formula
   desc "Enhanced Mule Tools CLI"
   homepage "https://www.enhanced-mule.com"
   url "https://repo1.maven.org/maven2/com/aeontronix/enhanced-mule/enhanced-mule-tools-cli/1.3.0-beta20/enhanced-mule-tools-cli-1.3.0-beta20-dist.tbz2"
-  sha256 "af534c3433ca4fb7934a7a85b441660834f3674815460bece8178fe6485bd22b"
+  sha256 "3a0fb048eaad2ff73f00d2db23e2a256d543ab04d6ff06fe1eed7f2e1a527fd5"
   head "https://github.com/enhanced-mule/homebrew-tools.git"
 
   def install
