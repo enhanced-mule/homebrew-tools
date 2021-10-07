16cde22dd7d9783d793b92dc450e759fdcadc54e
ymenager <ymenager@users.noreply.github.com>
2021-10-06 20:49:34 -0700

    update

--- formula/emt.rb
+++ formula/emt.rb
@@ -3,8 +3,8 @@ require "formula"
 class Emt < Formula
   desc "Enhanced Mule Tools CLI"
   homepage "https://www.enhanced-mule.com"
-  url "https://repo1.maven.org/maven2/com/aeontronix/enhanced-mule/enhanced-mule-tools-cli/1.3.0-beta22/enhanced-mule-tools-cli-1.3.0-beta22-dist.tbz2"
-  sha256 "c69d769206a8769760486aecc20ed5e97fb68fb725c9a33fcb5ff852f8a183b9"
+  url "https://repo1.maven.org/maven2/com/aeontronix/enhanced-mule/enhanced-mule-tools-cli/2.0.0-alpha1/enhanced-mule-tools-cli-2.0.0-alpha1-dist.tbz2"
+  sha256 "837be0f14c9b8ae85fee2ed78ca71b5a83a113db635da5181382b50c721c9247"
   head "https://github.com/enhanced-mule/homebrew-tools.git"
 
   def install
@@ -12,6 +12,6 @@ class Emt < Formula
     bin.write_exec_script libexec/"bin/emt"
   end
     test do
-      assert_match "1.3.0-beta22", shell_output("#{bin}/emt -V", 2)
+      assert_match "2.0.0-alpha1", shell_output("#{bin}/emt -V", 2)
     end
 end
